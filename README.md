f7e56fbec91169dfe1057e11bcf1e92c2582b124
Angel V <valdenegro.angel@gmail.com>
2019-12-23 12:34:49 -0800

    refactor: move constexpr out of functions; spacing
    Moved the constant expressions definining increments outside of the
    derivative functions and into a global space. Also added spaces in
    between asterisks when multiplying.

--- README.md
+++ README.md
@@ -15,7 +15,7 @@
 
 ARK is a personal project / C++ macro I wrote to numerically solve first and second order differential equations using the [Runge-Kutta Method](https://en.wikipedia.org/wiki/Runge–Kutta_methods). This algorithm is implemented using C++, which I chose for its speed and malleability. Examples of ARK solving the exponential ODE and the Simple Harmonic Oscillator ODE can be seen below!
 
-I also wrote a python file to plot the analytic solutions of these ODEs against the numerical solutions from ARK. This can be found as `rk4_analysis.py` in both folders.
+I also wrote a python file to plot the analytic solutions of these ODEs against the numerical solutions from ARK. This can be found as `rk4_analysis.py` in both folders. You may also find a file called `derivates.cpp` which calculates the value of a derivative at a certain point.
 
 This personal project is in active development, so please feel free to submit [issues](https://github.com/isoleph/ARK/issues) or a [pull request](https://github.com/isoleph/ARK/pulls)! 
 
--- derivatives.cpp
+++ derivatives.cpp
@@ -13,28 +13,29 @@
 #include <functional>
 
 
+constexpr const double increment = 0.00001;
+constexpr const double increment2 = 0.000000001;
+constexpr const double myPoint = 3.14159/4;
+
 inline double myFunction(double x) {
     return sin(x);
 }
 
+
 namespace dq {
     // definiton of the first derivative numerically
     double derivative(double x, std::function<double (double)> func) {
-        constexpr const double increment = 0.00001;
         return (func(x + increment) - func(x))/(increment);
     }
 
     // definition of the second derivative numerically
     double secondDerivative(double x, std::function<double (double)> func) {
-        constexpr const double increment = 0.00001;
-        constexpr const double increment2 = 0.000000001;
-        return (func(x + 2*increment) - 2*func(x + increment) + func(x))/(increment2);
+        return (func(x + 2 * increment) - 2 * func(x + increment) + func(x))/(increment2);
     }
 }
 
 
 int main() {
-    constexpr const double myPoint = 3.14159/4;
     std::cout << "Derivative at x: " << dq::derivative(myPoint, myFunction) << std::endl;
     std::cout << "Cosine at x: " << cos(myPoint) << std::endl;
     std::cout << "Negative Second Derivative at x: " << dq::secondDerivative(myPoint, myFunction) << std::endl;
