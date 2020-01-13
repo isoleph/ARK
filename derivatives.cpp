653ccd1467e47c625dd38586fb4d305be7c176e9
Angel V <valdenegro.angel@gmail.com>
2020-01-12 17:30:12 -0800

    feat: derivatives now available
    Commit adds a new version of the derivatives.cpp file that allows
    for numerically solving for derivatives. Functionality available through
    the iterator() function

--- derivatives.cpp
+++ derivatives.cpp
@@ -9,35 +9,47 @@
 *****************************************************/ 
 
 #include <iostream>
+#include <fstream>
 #include <cmath>
 #include <functional>
 
-
+// define constants to use for derivatives
 constexpr const double increment = 0.00001;
 constexpr const double increment2 = 0.000000001;
-constexpr const double myPoint = 3.14159/4;
 
+// function desired to differentiate
 inline double myFunction(double x) {
-    return sin(x);
+    return sin(x)/x;
 }
-
 
 namespace dq {
     // definiton of the first derivative numerically
     double derivative(double x, std::function<double (double)> func) {
         return (func(x + increment) - func(x))/(increment);
     }
-
     // definition of the second derivative numerically
     double secondDerivative(double x, std::function<double (double)> func) {
         return (func(x + 2 * increment) - 2 * func(x + increment) + func(x))/(increment2);
     }
 }
 
+// calculate over N points within x_0 -> x_f for f'(x)
+int iterator(double x_0, double x_f, int N) {
+    double dx = (x_f - x_0)/N; 
+    double x_i = 0;
+
+    std::ofstream outfile;
+    outfile.open("dvs.csv");
+    for (int i = 0; i <= N; i++) {
+        x_i += dx;
+        outfile << x_i << ", " << dq::derivative(x_i, myFunction) << std::endl;
+    }
+    outfile.close();
+    return 0;
+}
+
 
 int main() {
-    std::cout << "Derivative at x: " << dq::derivative(myPoint, myFunction) << std::endl;
-    std::cout << "Cosine at x: " << cos(myPoint) << std::endl;
-    std::cout << "Negative Second Derivative at x: " << dq::secondDerivative(myPoint, myFunction) << std::endl;
+    iterator(0, 10 * 3.14159, 100);
     return 0;
 }
