c5b897f684cb6c4373fc63e2732e626aa7a4135d
Angel Valdenegro <valdenegro.angel@gmail.com>
2020-05-21 23:14:57 -0700

    (refactor): removed inlines; added constructor

--- second_order/ARK2.cpp
+++ second_order/ARK2.cpp
@@ -7,6 +7,7 @@
 *   - Second-order solutions                        *
 * Author: Angel A. Valdenegro                       *
 * TODO: More tests with different functions         *
+* TODO: Ensure that vector lengths are same         *
 *****************************************************/ 
 
 #include <iostream>
@@ -16,59 +17,65 @@
 #include <functional>
 
 
+// import cmath definitions
+#define _USE_MATH_DEFINE
+
 // create shorthand for types later 
-#define pi 3.14159265
+constexpr double pi = M_PI;
 typedef std::function<double (double, double, double)> function;
 typedef std::vector<double> vector;
 
 
-// test function
+// function to integrate
 inline double myFunction(double x, double y, double yd) {
     return (-1 * y);
 }
 
 // R-K Class where the magic happens
 class RK4 {
+
     private: 
 
         // Store RK4 numbers
         double k1n, k2n, k3n, k4n;          // First-Order Runge-Kutta Numbers
-        double c1n, c2n, c3n, c4n;          // Define Second-Order Runge-Kutta Numbers
+        double c1n, c2n, c3n, c4n;          // Second-Order Runge-Kutta Numbers
 
 
         //** Functions to calculate Second-Order RK Numbers **//
-        inline double c1 (double ydn, double h) {                                              // c1n
+
+        double c1 (double ydn, double h) {                          // c1n
             c1n = ydn;
             return c1n;
         }
-        inline double c2 (double ydn, double h) {                                              // c2n
+        double c2 (double ydn, double h) {                          // c2n
             c2n = ydn + 0.5 * h * k1n;
             return c2n;
         }
-        inline double c3 (double ydn, double h ) {                                             // c3n
+        double c3 (double ydn, double h ) {                         // c3n
             c3n = ydn + 0.5 * h * k2n;
             return c3n;
         }
-        inline double c4 (double ydn, double h) {                                              // c4n
+        double c4 (double ydn, double h) {                          // c4n
             c4n = ydn + h * k3n;
             return c4n;
         }
 
 
         //** Functions to calculate First-Order RK Numbers**//
-        inline double k1 (double t, double y, double ydn, double h, function func) {           // k1n
+
+        double k1 (double t, double y, double ydn, double h, function func) {           // k1n
             k1n = func(t, y, ydn);
             return k1n;
         }
-        inline double k2 (double t, double y, double ydn, double h, function func) {           // k2n
+        double k2 (double t, double y, double ydn, double h, function func) {           // k2n
             k2n = func(t + h/2, y + h/2 * c1(ydn, h), ydn);
             return k2n;
         }
-        inline double k3 (double t, double y, double ydn, double h, function func) {           // k3n
+        double k3 (double t, double y, double ydn, double h, function func) {           // k3n
             k3n = func(t + h/2, y + h/2 * c2(ydn, h), ydn);
             return k3n;
         }
-        inline double k4 (double t, double y, double ydn, double h, function func) {           // k4n
+        double k4 (double t, double y, double ydn, double h, function func) {           // k4n
             k4n = func(t + h, y + h * c3(ydn, h), ydn);
             return k4n;
         }
@@ -79,7 +86,7 @@ class RK4 {
         vector yvalues;
 
         // initial t-value t_0, final t-value t_f, initial y-value y_0, numbers of steps n
-        vector iterator(double t_0, double t_f, double y_0, double yd_0, int n = 100, function func = myFunction) {
+        RK4(double t_0, double t_f, double y_0, double yd_0, int n = 100, function func = myFunction) {
 
             double h = (t_f - t_0)/n;           // calculate incremental distances
             double y_n = y_0;                   // initial y value
@@ -111,24 +118,24 @@ class RK4 {
 
             xvalues = xpoints;
             yvalues = ypoints;
-            return yvalues;
         }
 };
 
 
 // create csv with numerical solution
+
 int main() {
-    RK4 object;
+
     // remember proper initial conditions!!
-    vector results = object.iterator(0, 2 * pi, 0, 1, 500, myFunction);
+    RK4 object = RK4(0, 2 * pi, 0, 1, 500, myFunction);
+    vector results = object.yvalues;
 
     std::ofstream outfile;
     outfile.open("ARK2.csv");
+
     for (int i = 0; i < results.size(); i++) {
         outfile << object.xvalues[i] << ", " <<  object.yvalues[i] << std::endl;
     }
     outfile.close();
     return 0;
 }
-
-
